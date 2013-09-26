ef4fc6db245f8427fbdb932ef0d943b298e20986
sanborn <sanborn@sanborn-SVE14112ENB.(none)>
2013-09-26 11:56:30 +0530

    Test changes

--- Vindowshop.php
+++ Vindowshop.php
@@ -28,101 +28,101 @@ class Vindowshop
 	    * @param string $apiKey for vindowshop application
     */
 
-	function __construct($appId, $apiKey){
-		$this->setAppId($appId);
-		$this->setApiKey($apiKey);
-	}
+    function __construct($appId, $apiKey){
+    	$this->setAppId($appId);
+    	$this->setApiKey($apiKey);
+    }
 
 	/**
 		* Get the version of the API wrapper.
 		* @return string Version of the API wrapper.
 	*/
-	public function getVersion(){
-		return self::version;
-	}
+		public function getVersion(){
+			return self::version;
+		}
 
 	/**
 		* Initializing user app ID
 		* @param string $appId for Vindowshop application
 	*/
 
-	private function setAppId($appId){
-		$this->app_id = (string)$appId;
-	}
+		private function setAppId($appId){
+			$this->app_id = (string)$appId;
+		}
 
 	/**
 		* Initializing user api Key
 		* @param string $apiKey for Vindowshop application
 	*/
 
-	private function setApiKey($apiKey){
-		$this->api_key = (string)$apiKey;
-	}
+		private function setApiKey($apiKey){
+			$this->api_key = (string)$apiKey;
+		}
 
 
 	/**
 		* Authenticate the application.
 		* @return array PHP array of the JSON response.
 	*/
-	public function apiAuth(){
-		$response = $this->apiRequest('auth/', $data = array('appId' => $this->app_id, 'apiKey' => $this->api_key));
-		$array = json_decode($response, true);
-		if(!$array['error']){
-			$this->app_token = $array['token'];
+		public function apiAuth(){
+			$response = $this->apiRequest('auth/', $data = array('appId' => $this->app_id, 'apiKey' => $this->api_key));
+			$array = json_decode($response, true);
+			if(!$array['error']){
+				$this->app_token = $array['token'];
+			}
+			else{
+				throw new Exception($array['error']);
+			}
 		}
-		else{
-			throw new Exception($array['error']);
-		}
-	}
 
 	/**
 		* This method helps to get the authorization information
 		* @return array of appId and appToken
 	*/
-	protected function getAuthInfo(){
-		if($this->app_token)
-			return array('appId'=>$this->app_id, 'appToken'=>$this->app_token);
-		else
-			return null;
-	}
+		protected function getAuthInfo(){
+			if($this->app_token)
+				return array('appId'=>$this->app_id, 'appToken'=>$this->app_token);
+			else
+				return null;
+		}
 
 	/**
 		* Extracting and Sending image for processing from a post
 		* @param string $string The post where the images will be extracted from
 	*/
-	public function sendImages($string){
-		$images = $this->getImagesUrls($string);
-		$data = array('from_wrapper',$images);
-		$response = $this->apiRequest('',$data);
-	}
+		public function sendImages($string){
+			$images = $this->getImagesUrls($string);
+			$data = array('from_wrapper',$images);
+			$response = $this->apiRequest('',$data);
+		}
 
-	
+		
 	/**
 		* Getting all image urls from the string
 		* @param string $string The post where the image urls will be extracted from
 		* @return array $matches will return the all the image urls containing in a string
 	*/
 
-	private function getImagesUrls($string){
-		$imageTags = $this->getImageTags($string);
-		$imageTags = implode(" ", $imageTags);
-	    $regex = '/https?\:\/\/[^\" ]+/i';
-    	preg_match_all($regex, $imageTags, $matches);
-    	return ($matches[0]);
-	}
+		private function getImagesUrls($string){
+			$imageTags = $this->getImageTags($string);
+			$imageTags = implode(" ", $imageTags);
+			$regex = '/https?\:\/\/[^\" ]+/i';
+			preg_match_all($regex, $imageTags, $matches);
+			return ($matches[0]);
+		}
 
-	
+		
 	/**
 		* Getting all image tags in an array from the string
 		* @param string $string The post where the image tagss will be extracted from
 		* @return array $matches will give all the image tags in a string i.e. <img> tags 
 	*/
 
-	private function getImageTags($string){
-		$regex = '/<img\s+.*?src=[\"\']?([^\"\' >]*)[\"\']?[^>]*>/i';
-		preg_match_all($regex, $string, $matches);
-		return ($matches[0]);
-	}
+		private function getImageTags($string){
+			$regex = '/<img\s+.*?src=[\"\']?([^\"\' >]*)[\"\']?[^>]*>/i';
+			preg_match_all($regex, $string, $matches);
+			return ($matches[0]);
+		}
 
 	/**
 		* Create the absolute path for the request.
@@ -130,9 +130,9 @@ class Vindowshop
 		* @param string $path The relative path.
 		* @return string $url.$path the entire path to send request
 	*/
-	private function buildPath($url, $path){
-		return $url . $path;
-	}
+		private function buildPath($url, $path){
+			return $url . $path;
+		}
 
 
 	/**
@@ -141,11 +141,11 @@ class Vindowshop
 		* @param string $url of the image
 		* @return Response
 	*/
-	public function getMatches($url){
-		$data = array($url,'Women Topwear',292,438,136,189,64,64);
-		$response = $this->apiRequest('fetchprod',$data);
-		die(var_dump($response));
-	}
+		public function getMatches($url){
+			$data = array($url,'Women Topwear',292,438,136,189,64,64);
+			$response = $this->apiRequest('fetchprod',$data);
+			die(var_dump($response));
+		}
 
 	/**
 		* This method helps users to get images uploaded by them only
@@ -153,36 +153,36 @@ class Vindowshop
 		* @return $response json encoded string containing all the urls o the immages uploaded by current appId
 	*/
 
-	public function getMyImages(){
-		$blank_array = array();
-		$response = $this->apiRequest('getmyimages/',$blank_array); 
-		return $response;
-	}
+		public function getMyImages(){
+			$blank_array = array();
+			$response = $this->apiRequest('getmyimages/',$blank_array); 
+			return $response;
+		}
 
 	/**
 		* This methos helps user to create supprotng DOM of vindowshop on their website
 		* @return $html the DOM to be added
 	*/
 
-	public function createJS(){
-		$html = '<div id="basic-modal-content"></div>';
-		$html .= '<script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>';
-		$html .= '<script type="text/javascript" src="https://dl.dropboxusercontent.com/u/107817493/vindowshop/js/jquery.js"></script>';
-		$html .= '<script type="text/javascript" src="https://dl.dropboxusercontent.com/u/107817493/vindowshop/js/jquery.simplemodal.js"></script>';
-		$html .= '<script type="text/javascript" src="https://dl.dropboxusercontent.com/u/107817493/vindowshop/js/vindowshop.js"></script>';
-		$html .= '<script type="text/javascript" >';
-		$html .= 'var img_array = '.str_replace('"', "'", $this->getMyImages()).';';
-		$html .= 'var img = document.body.getElementsByTagName("img");';
-		$html .= 'var i = 0;';
-		$html .= 'while (i < img.length) {; var pos = inArray(img[i].src, img_array);';
-		$html .= 'if(pos != \'not found\'){var image_url = \'"\'+img[i].src+\'"\'; var new_html = \'<a href="javascript:void(0)"><img onmouseover="javascript:lights_in(this)" onclick="javascript:select_gender(this,image_url)" onmouseout="javascript:lights_out(this)" style="opacity: 0.4; position: absolute; z-index: 1; top: 15px; right: 30px; max-height:40px" src="http://www.f6s.com/pictures/profiles/17/1641/164049_th2.jpg"></a>\';';
-    	$html .= 'img[i].parentNode.setAttribute(\'style\',\'display: inline-block;position: relative;\');';
-    	$html .= 'img[i].parentNode.innerHTML = img[i].parentNode.innerHTML+new_html;';
-    	$html .= '}i++;}</script>';
-    	$html .= '<link rel="stylesheet" href="https://dl.dropboxusercontent.com/u/107817493/vindowshop/css/basic.css" type="text/css" media="all" />';
-		$html .= '<link rel="stylesheet" href="https://dl.dropboxusercontent.com/u/107817493/vindowshop/css/VindowShop.css" type="text/css" media="all" />';
-		return $html;
-	}
+		public function createJS(){
+			$html = '<div id="basic-modal-content"></div>';
+			$html .= '<script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>';
+			$html .= '<script type="text/javascript" src="https://dl.dropboxusercontent.com/u/107817493/vindowshop/js/jquery.js"></script>';
+			$html .= '<script type="text/javascript" src="https://dl.dropboxusercontent.com/u/107817493/vindowshop/js/jquery.simplemodal.js"></script>';
+			$html .= '<script type="text/javascript" src="https://dl.dropboxusercontent.com/u/107817493/vindowshop/js/vindowshop.js"></script>';
+			$html .= '<script type="text/javascript" >';
+			$html .= 'var img_array = '.str_replace('"', "'", $this->getMyImages()).';';
+			$html .= 'var img = document.body.getElementsByTagName("img");';
+			$html .= 'var i = 0;';
+			$html .= 'while (i < img.length) {; var pos = inArray(img[i].src, img_array);';
+			$html .= 'if(pos != \'not found\'){var image_url = \'"\'+img[i].src+\'"\'; var new_html = \'<a href="javascript:void(0)"><img onmouseover="javascript:lights_in(this)" onclick="javascript:select_gender(this,image_url)" onmouseout="javascript:lights_out(this)" style="opacity: 0.4; position: absolute; z-index: 1; top: 15px; right: 30px; max-height:40px" src="http://www.f6s.com/pictures/profiles/17/1641/164049_th2.jpg"></a>\';';
+			$html .= 'img[i].parentNode.setAttribute(\'style\',\'display: inline-block;position: relative;\');';
+			$html .= 'img[i].parentNode.innerHTML = img[i].parentNode.innerHTML+new_html;';
+			$html .= '}i++;}</script>';
+			$html .= '<link rel="stylesheet" href="https://dl.dropboxusercontent.com/u/107817493/vindowshop/css/basic.css" type="text/css" media="all" />';
+			$html .= '<link rel="stylesheet" href="https://dl.dropboxusercontent.com/u/107817493/vindowshop/css/VindowShop.css" type="text/css" media="all" />';
+			return $html;
+		}
 
 
 	/**
@@ -192,32 +192,32 @@ class Vindowshop
 		* @return json $result Json the reply from the request
 	*/
 
-	private function apiRequest($path, array $data = null){
-		$path = (string) $path;
-		$data = (array) $data;
-		$data[] = $this->getAuthInfo();
-		$url = 	$this->buildPath(self::API_URL,$path);
-		$params = json_encode($data);
+		private function apiRequest($path, array $data = null){
+			$path = (string) $path;
+			$data = (array) $data;
+			$data[] = $this->getAuthInfo();
+			$url = 	$this->buildPath(self::API_URL,$path);
+			$params = json_encode($data);
+			
+			$ch = curl_init($url);
+
+			curl_setopt($ch, CURLOPT_POST, 1);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			$result = curl_exec($ch);
+
+			curl_close($ch);
+
+			return $result;
+		}
 		
-		$ch = curl_init($url);
-
-		curl_setopt($ch, CURLOPT_POST, 1);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		$result = curl_exec($ch);
-
-		curl_close($ch);
-
-		return $result;
 	}
-	
-}
 
 
 // Testing
 
-$instance = new Vindowshop('123456789','987654321');
-$instance->apiAuth();
+	$instance = new Vindowshop('123456789','987654321');
+	$instance->apiAuth();
 //$instance->sendImages($string); // String is the entire post including image urls, just passing the entire post will make it work
 $instance->getMyImages(); // Get all the image of appId 123456789
 $htm = $instance->createJS(); // Creating DOM via this function (Normally this function should be called in the footer section)
